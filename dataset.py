74ab3b47657fc16d8e7d853cb9a9804cdf2cfa7d
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-03-25 15:34:48 +0900

    Fix issue #1

--- dataset.py
+++ dataset.py
@@ -3,34 +3,58 @@ import numpy as np
 import musdb
 import random
 from model import TasNetParam
+from tqdm import tqdm
 
-mus = list(musdb.DB(root="D:/Datasets/musdb18").tracks)
 
-def musdb_generator(param: TasNetParam, batch_size: int, n: int):
+def decode_source(track):
+    rtn = {"audio": (track.audio[:, 0], track.audio[:, 1])}
+    rtn["length"] = rtn["audio"][0].shape[-1]
+    for target in ("vocals", "drums", "bass", "other"):
+        audio = track.targets[target].audio
+        rtn[target] = (audio[:, 0], audio[:, 1])
+    return rtn
+
+
+def musdb_generator(param: TasNetParam, num_songs: int, batch_size: int, n: int):
+    db = list(musdb.DB(root="D:/Datasets/musdb18", subsets="train").tracks)
+    random.shuffle(db)
+    db = db[:num_songs]
+
+    print("Decoding dataset...")
+    mus = []
+    for track in tqdm(db):
+        mus.append(decode_source(track))
+    print("Decoding dataset...done")
+
     shape = (param.K, param.L)
-    duration = param.K * param.L / 44100
+    duration = param.K * param.L
     for _ in range(n):
         X = []
         Y = []
         for _ in range(batch_size):
             track = random.choice(mus)
-            track.chunk_duration = duration
-            track.chunk_start = random.uniform(0, track.duration - track.chunk_duration)
-            x = track.audio.T
-            x_0, x_1 = np.reshape(x[0], shape), np.reshape(x[1], shape)
-            y = [
-                track.targets[target].audio.T
+            start = random.randint(0, track["length"] - duration)
+            end = start + duration
+            x_0 = np.reshape(track["audio"][0][start:end], shape)
+            x_1 = np.reshape(track["audio"][1][start:end], shape)
+            y_0 = [
+                np.reshape(track[target][0][start:end], shape)
                 for target in ("vocals", "drums", "bass", "other")
             ]
-            y_0 = np.array([np.reshape(track[0], shape) for track in y])
-            y_1 = np.array([np.reshape(track[1], shape) for track in y])
+            y_1 = [
+                np.reshape(track[target][1][start:end], shape)
+                for target in ("vocals", "drums", "bass", "other")
+            ]
             X.extend([x_0, x_1])
             Y.extend([y_0, y_1])
         yield np.array(X), np.array(Y)
 
-def make_dataset(param: TasNetParam, batch_size: int, n: int):
-    return tf.data.Dataset.from_generator(lambda: musdb_generator(param, batch_size, n),
+
+def make_dataset(param: TasNetParam, num_songs: int, batch_size: int, n: int):
+    return tf.data.Dataset.from_generator(lambda: musdb_generator(param, num_songs, batch_size, n),
                                           output_signature=(
-                                              tf.TensorSpec(shape=(batch_size * 2, param.K, param.L)),
-                                              tf.TensorSpec(shape=(batch_size * 2, param.C, param.K, param.L))
-                                          ))
+                                              tf.TensorSpec(
+                                                  shape=(batch_size * 2, param.K, param.L)),
+                                              tf.TensorSpec(
+                                                  shape=(batch_size * 2, param.C, param.K, param.L))
+    ))
