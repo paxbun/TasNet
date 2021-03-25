86214efa3f384b935648550b50d4062b1d2d8700
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-03-25 15:35:06 +0900

    Fix wrong implementations

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import tensorflow as tf
+from os import path, listdir
 from dataset import make_dataset
 from model import TasNet, TasNetParam
 
 param = TasNetParam(N=500, L=40, H=1000, K=10, C=4, g=1.0, b=0.0)
-dataset = make_dataset(param, 10, 100)
 model = TasNet.make(param)
-model.fit(dataset)
-model.save("model.h5")
+
+epoch = 0
+if path.exists("training"):
+    checkpoints = [name for name in listdir("training") if "ckpt" in name]
+    checkpoints.sort()
+    checkpoint_name = checkpoints[-1].split(".")[0]
+    epoch = int(checkpoint_name) + 1
+    model.load_weights(f"training/{checkpoint_name}.ckpt")
+
+checkpoint_path = "training/{epoch:05d}.ckpt"
+while True:
+    print("Epoch:", epoch)
+    dataset = make_dataset(param, 5, 100, 1000)
+    model.fit(dataset)
+    model.save_weights(checkpoint_path.format(epoch=epoch))
+    epoch += 1
--- model.py
+++ model.py
@@ -8,19 +8,31 @@ class TasNetParam:
         self.N, self.L, self.H, self.K, self.C = N, L, H, K, C
         self.g, self.b = g, b
 
+    def get_config(self):
+        return {
+            "N": self.N,
+            "L": self.L,
+            "H": self.H,
+            "K": self.K,
+            "C": self.C,
+            "g": self.g,
+            "b": self.b
+        }
+
 
 class EncodingNet(tf.keras.layers.Layer):
     def __init__(self, param: TasNetParam, **kwargs):
         super(EncodingNet, self).__init__(**kwargs)
         self.param = param
-        self.input_reshape = tf.keras.layers.Reshape(target_shape=(self.param.K, self.param.L, 1))
+        self.input_reshape = tf.keras.layers.Reshape(
+            target_shape=(self.param.K, self.param.L, 1))
         self.relu = tf.keras.layers.Conv2D(self.param.N,
                                            kernel_size=(1, self.param.L),
                                            activation="relu",
                                            padding="valid")
         self.sigmoid = tf.keras.layers.Conv2D(self.param.N,
                                               kernel_size=(1, self.param.L),
-                                              activation="relu",
+                                              activation="sigmoid",
                                               padding="valid")
         self.multiply = tf.keras.layers.Multiply()
         self.output_reshape = tf.keras.layers.Reshape(
@@ -32,56 +44,60 @@ class EncodingNet(tf.keras.layers.Layer):
     def call(self, inputs):
         outputs = self.input_reshape(inputs)
         relu = self.relu(outputs)
-        sigmoid = self.relu(outputs)
+        sigmoid = self.sigmoid(outputs)
         outputs = self.multiply([relu, sigmoid])
         outputs = self.output_reshape(outputs)
         outputs = self.normalization(outputs)
         outputs = self.transform(outputs)
         return outputs
 
+    def get_config(self):
+        return self.param.get_config()
 
-class SeparationNetLSTM(tf.keras.layers.Layer):
+
+class SeparationNet(tf.keras.layers.Layer):
     def __init__(self, param: TasNetParam, **kwargs):
-        super(SeparationNetLSTM, self).__init__(**kwargs)
+        super(SeparationNet, self).__init__(**kwargs)
         self.param = param
         self.L1 = tf.keras.layers.LSTM(self.param.H, return_sequences=True)
         self.L2 = tf.keras.layers.LSTM(self.param.H, return_sequences=True)
         self.L3 = tf.keras.layers.LSTM(self.param.H, return_sequences=True)
         self.L4 = tf.keras.layers.LSTM(self.param.H, return_sequences=True)
-        self.SkipConn = tf.keras.layers.Add()
+        self.skip_conn = tf.keras.layers.Add()
+        self.M = tf.keras.layers.Dense(self.param.N * self.param.C)
+        self.softmax = tf.keras.layers.Softmax(axis=-2)
+        self.D = tf.keras.layers.Multiply()
+        self.reshape = tf.keras.layers.Reshape((self.param.K, self.param.C, self.param.N))
 
     def call(self, inputs):
         L1_outputs = self.L1(inputs)
         L2_outputs = self.L2(L1_outputs)
         L3_outputs = self.L3(L2_outputs)
         L4_outputs = self.L4(L3_outputs)
-        outputs = self.SkipConn([L2_outputs, L4_outputs])
+        outputs = self.skip_conn([L2_outputs, L4_outputs])
+        outputs = self.M(outputs)
+        inputs = tf.keras.layers.concatenate([inputs for i in range(self.param.C)], axis=-1)
+        outputs = self.D([inputs, outputs])
+        outputs = self.reshape(outputs)
         return outputs
 
-
-class SeparationNetDense(tf.keras.layers.Layer):
-    def __init__(self, param: TasNetParam, **kwargs):
-        super(SeparationNetDense, self).__init__(**kwargs)
-        self.param = param
-        self.M = tf.keras.layers.Dense(self.param.N)
-        self.D = tf.keras.layers.Multiply()
-
-    def call(self, inputs):
-        W, LSTM = inputs
-        outputs = self.M(LSTM)
-        outputs = self.D([W, outputs])
-        return outputs
-
+    def get_config(self):
+        return self.param.get_config()
 
 class DecodingNet(tf.keras.layers.Layer):
     def __init__(self, param: TasNetParam, **kwargs):
         super(DecodingNet, self).__init__(**kwargs)
         self.param = param
         self.basis = tf.keras.layers.Dense(self.param.L)
+        self.permute = tf.keras.layers.Permute((2, 1, 3))
 
     def call(self, inputs):
         outputs = self.basis(inputs)
+        outputs = self.permute(outputs)
         return outputs
+
+    def get_config(self):
+        return self.param.get_config()
 
 
 class SiSNR(tf.keras.losses.Loss):
@@ -93,7 +109,8 @@ class SiSNR(tf.keras.losses.Loss):
         s_hat = tf.reshape(s_hat, s.shape)
         s_target = s * (tf.reduce_sum(tf.multiply(s, s_hat)) / tf.norm(s))
         e_noise = s_hat - s_target
-        result = 20 * tf.math.log(tf.norm(e_noise) / (tf.norm(s_target) + 1e-10) + 1e-10)
+        result = 20 * tf.math.log(tf.norm(e_noise) /
+                                  (tf.norm(s_target) + 1e-10) + 1e-10)
         return result
 
 
@@ -109,22 +126,11 @@ class TasNet(tf.keras.Model):
         super(TasNet, self).__init__(**kwargs)
         self.param = param
         self.encoding = EncodingNet(self.param)
-        self.separation_lstm = SeparationNetLSTM(self.param)
-        self.separation_dense_list = [
-            SeparationNetDense(self.param)
-            for i in range(self.param.C)
-        ]
+        self.separation = SeparationNet(self.param)
         self.decoding = DecodingNet(self.param)
 
     def call(self, inputs):
         outputs = self.encoding(inputs)
-        lstm_outputs = self.separation_lstm(outputs)
-        separated_list = [
-            separation_dense([outputs, lstm_outputs])
-            for separation_dense in self.separation_dense_list
-        ]
-        decoded_list = [
-            self.decoding(separated)
-            for separated in separated_list
-        ]
-        return tf.keras.layers.concatenate(decoded_list, axis=-3)
+        separated = self.separation(outputs)
+        decoded = self.decoding(separated)
+        return decoded
